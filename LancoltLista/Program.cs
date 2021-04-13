384ede6827e786ad3bbf3e80a3710c215c8ba79c
Your Name <you@example.com>
2021-04-13 14:42:12 +0200

    First commit

---
+++ LancoltLista/Program.cs
@@ -0,0 +1,385 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace LancoltLista
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            
+        }
+    }
+    public enum Pokemontype
+    {
+        Normal,Fire, Water, Grass, Electric, Ice, Fighting, Poison, Ground, Flying, Psychic, Bug, Rock, Ghost, Dark, Dragon, Steel, Fairy
+    }
+    public class Pokemon
+    {
+        public string Name { get; set; }
+        public Pokemontype  Type { get; set; }
+        public int Health { get; set; }
+        public bool Rare { get; set; }
+        public int  Generation { get; set; }
+        public int StrengthPoint { get; set; }
+        public Pokemon Stronger { get; set; }
+
+        public Pokemon(string name, Pokemontype type, int health,bool rare,int generation,int strengthPoint)
+        {
+            Name = name;
+            Type = type;
+            Health = health;
+            Rare = rare;
+            Generation = generation;
+            StrengthPoint = strengthPoint;
+        }
+        public override bool Equals(object obj)
+        {
+            Pokemon ez = this;
+            Pokemon masik = (Pokemon)obj;
+            return ez.GetHashCode() == masik.GetHashCode();
+        }
+        public override int GetHashCode()
+        {
+            return Name.GetHashCode() + Type.GetHashCode() + Health.GetHashCode() + Rare.GetHashCode() + Generation.GetHashCode() + StrengthPoint.GetHashCode();
+        }
+
+    }
+    public class PokemonLinkedList
+    {
+        public Pokemon head;
+        public void Beszuras(Pokemon pokemon)
+        {        
+            if (head == null)
+            {
+                pokemon.Stronger = null;
+                head = pokemon;
+            }else if (head.StrengthPoint > pokemon.StrengthPoint)
+            {
+                pokemon.Stronger = head;
+                head = pokemon;
+            }
+            else
+            {
+                Pokemon p = head;
+                Pokemon e = null;
+                while (p != null && p.StrengthPoint <= pokemon.StrengthPoint)
+                {
+                    if (p.StrengthPoint == pokemon.StrengthPoint)
+                    {
+                        if (p.Equals(pokemon))
+                        {
+                            throw new IlyenMarLetezikException();
+                        }
+                    }
+                    e = p;
+                    p = p.Stronger;
+                }             
+                if (p == null)
+                {
+                    pokemon.Stronger = null;
+                    e.Stronger = pokemon;
+                }
+                else
+                {
+                    pokemon.Stronger = p;
+                    e.Stronger = pokemon;
+                }
+            }
+        }
+
+        public Pokemon Kereses(string nev)
+        {
+            Pokemon p = head;
+            while (p != null && p.Name != nev)
+            {
+                p = p.Stronger;
+            }
+            if (p!= null)
+            {
+                return p;
+            }
+            else
+            {
+                throw new NincsIlyenPokemonException();
+            }
+        }
+
+        public void Torles(Pokemon pokemon, bool tartalomszerint)
+        {
+            Pokemon p = head;
+            Pokemon e = null;
+            if (!tartalomszerint)
+            {
+                while (p != null && pokemon != p)
+                {
+                    e = p;
+                    p = p.Stronger;
+                }
+                if (p != null)
+                {
+                    if (e == null)
+                    {
+                        head = p.Stronger;
+                    }
+                    else
+                    {
+                        e.Stronger = p.Stronger;
+                    }
+                }
+                else
+                {
+                    throw new NincsIlyenPokemonException();
+                }
+            }
+            else
+            {
+                while (p != null && !pokemon.Equals(p))
+                {
+                    e = p;
+                    p = p.Stronger;
+                }
+                if (p != null)
+                {
+                    if (e == null)
+                    {
+                        head = p.Stronger;
+                    }
+                    else
+                    {
+                        e.Stronger = p.Stronger;
+                    }
+                }
+                else
+                {
+                    throw new NincsIlyenPokemonException();
+                }
+            }
+           
+        }
+        public void Torles(string nev)
+        {
+            Pokemon e = null;
+            Pokemon p = head;
+            while (p != null && p.Name != nev)
+            {
+                e = p;
+                p = p.Stronger;
+            }
+            if (p!=null)
+            {
+                if (e==null)
+                {
+                    head = p.Stronger;
+                }
+                else
+                {
+                    e.Stronger = p.Stronger;
+                }
+            }
+            else
+            {
+                throw new NincsIlyenPokemonException();
+            }
+        }
+
+        public PokemonLinkedList Szures(string nevTartalmaz)
+        {
+            PokemonLinkedList outp = new PokemonLinkedList();
+            Pokemon p = this.head;
+            while (p!=null)
+            {
+                if (p.Name.Contains(nevTartalmaz))
+                {
+                    Pokemon masolatPokemon = new Pokemon(p.Name, p.Type, p.Health, p.Rare, p.Generation, p.StrengthPoint);
+                    outp.Beszuras(masolatPokemon);
+                }
+                p = p.Stronger;
+            }
+            return outp;
+        }
+        public PokemonLinkedList Szures(int strengthPoint, bool higher)
+        {
+            PokemonLinkedList outp = new PokemonLinkedList();
+            Pokemon p = this.head;
+            while (p != null)
+            {
+                if (higher && p.StrengthPoint > strengthPoint)
+                {
+                    Pokemon masolatPokemon = new Pokemon(p.Name, p.Type, p.Health, p.Rare, p.Generation, p.StrengthPoint);
+                    outp.Beszuras(masolatPokemon);
+                }
+                else if (!higher && p.StrengthPoint < strengthPoint)
+                {
+                    Pokemon masolatPokemon = new Pokemon(p.Name, p.Type, p.Health, p.Rare, p.Generation, p.StrengthPoint);
+                    outp.Beszuras(masolatPokemon);
+                }
+                p = p.Stronger;
+            }
+            return outp;
+        }
+        public PokemonLinkedList Szures(Pokemontype type)
+        {
+            PokemonLinkedList outp = new PokemonLinkedList();
+            Pokemon p = this.head;
+            while (p != null)
+            {
+                if (p.Type == type)
+                {
+                    Pokemon masolatPokemon = new Pokemon(p.Name, p.Type, p.Health, p.Rare, p.Generation, p.StrengthPoint);
+                    outp.Beszuras(masolatPokemon);
+                }
+                p = p.Stronger;
+            }
+            return outp;
+        }
+
+        public event PokemonLinkedListEvent bejarasEvent;
+        public void Bejaras()
+        {
+            Pokemon p = this.head;
+            while (p!=null)
+            {
+                bejarasEvent?.Invoke(p);
+                p = p.Stronger;
+            }
+
+
+        }
+
+        public PokemonLinkedList Metszet(PokemonLinkedList masikLista)
+        {
+            PokemonLinkedList outp = new PokemonLinkedList();
+            Pokemon p = this.head;
+            Pokemon m = masikLista.head;
+            while (p!= null && m!= null)
+            {
+                if (p.Equals(m))
+                {
+                    Pokemon pokemoncopy = new Pokemon(p.Name, p.Type, p.Health, p.Rare, p.Generation, p.StrengthPoint);
+                    outp.Beszuras(pokemoncopy);
+                    p = p.Stronger;
+                    m = m.Stronger;
+                }
+                else if (p.StrengthPoint == m.StrengthPoint || p.StrengthPoint > m.StrengthPoint)
+                {
+                    if (p.StrengthPoint == m.StrengthPoint && p.Stronger.StrengthPoint == m.StrengthPoint)
+                    {
+                        p = p.Stronger;
+                    }
+                    else
+                    {
+                        m = m.Stronger;
+                    }
+
+                }
+                else
+                {
+                    p = p.Stronger;
+                }
+            }
+            return outp;
+        }
+
+        public PokemonLinkedList Unio(PokemonLinkedList masikLista)
+        {
+            PokemonLinkedList outp = new PokemonLinkedList();
+            Pokemon p = this.head;
+            Pokemon m = masikLista.head;
+            while (p!=null)
+            {
+                try
+                {
+                    Pokemon pokemoncopy = new Pokemon(p.Name, p.Type, p.Health, p.Rare, p.Generation, p.StrengthPoint);
+                    outp.Beszuras(pokemoncopy);
+                    p = p.Stronger;                 
+                }
+                catch (IlyenMarLetezikException e)
+                {
+
+                }
+                
+            }
+            while (m!=null)
+            {
+                try
+                {
+                    Pokemon pokemoncopy = new Pokemon(m.Name, m.Type, m.Health, m.Rare, m.Generation, m.StrengthPoint);
+                    outp.Beszuras(pokemoncopy);
+                }
+                catch (IlyenMarLetezikException e)
+                {
+
+                }
+                m = m.Stronger;
+            }
+            return outp;
+        }
+
+        public PokemonLinkedList Kulonbseg(PokemonLinkedList masikLista)
+        {
+            PokemonLinkedList outp = new PokemonLinkedList();
+            Pokemon p = this.head;
+            Pokemon m = masikLista.head;
+            if (m == null)
+            {
+                return this;
+            }
+            else
+            {
+                while (p!=null && m!=null)
+                {
+                    if (p.Equals(m))
+                    {
+                        p = p.Stronger;
+                        m = m.Stronger;
+                    }
+                    else if (p.StrengthPoint < m.StrengthPoint)
+                    {
+
+                        Pokemon pokemoncopy = new Pokemon(p.Name, p.Type, p.Health, p.Rare, p.Generation, p.StrengthPoint);
+                        outp.Beszuras(pokemoncopy);
+                        p = p.Stronger;
+                    }
+                    else if (p.StrengthPoint == m.StrengthPoint)
+                    {
+                        m = m.Stronger;
+                    }
+                    else
+                    {
+                        m = m.Stronger;
+                    }
+                }
+                while (p!=null)
+                {
+                    Pokemon pokemoncopy = new Pokemon(p.Name, p.Type, p.Health, p.Rare, p.Generation, p.StrengthPoint);
+                    outp.Beszuras(pokemoncopy);
+                    p = p.Stronger;
+                }
+            }
+            return outp;
+
+        }
+
+    }
+    public delegate void PokemonLinkedListEvent(Pokemon pokemon);
+
+    public class IlyenMarLetezikException : Exception
+    {
+        public IlyenMarLetezikException(): base("[HIBA] Ilyen mar letezik!")
+        {
+                
+        }
+    }
+    public class NincsIlyenPokemonException : Exception
+    {
+        public NincsIlyenPokemonException():base("A keresett pokemon nem talalhato")
+        {
+
+        }
+    }
+
+}
---
+++ TestLibary/Class1.cs
@@ -0,0 +1,313 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using LancoltLista;
+using NUnit.Framework;
+
+namespace TestLibary
+{
+    [TestFixture]
+    public class TesterClass
+    {
+        PokemonLinkedList lista;
+        [SetUp]
+        public void Init()
+        {
+            lista = new PokemonLinkedList();
+        }
+
+        //Beszuras tesztek
+        [TestCase]
+        public void EqualsTeszt()
+        {
+            Pokemon pokemon1 = new Pokemon("Pikachu", Pokemontype.Electric, 31, false, 1, 20);
+            Pokemon pokemon2 = new Pokemon("Pikachu2", Pokemontype.Electric, 31, false, 1, 20);
+            Assert.That(!(pokemon1.Equals(pokemon2)));
+        }
+        public void EqualsTeszt2()
+        {
+           lista.Beszuras(new Pokemon("Pikachu", Pokemontype.Electric, 31, false, 1, 20));
+           
+            Assert.That(lista.head.Equals(new Pokemon("Pikachu", Pokemontype.Electric, 31, false, 1, 20)));
+        }
+        [TestCase]
+        public void UresTorzsBeszuras()
+        {
+            lista.Beszuras(new Pokemon("Pikachu", Pokemontype.Electric,20 , false, 1, 40));
+            Assert.That(lista.head.Equals(new Pokemon("Pikachu", Pokemontype.Electric, 20, false, 1, 40)));
+        }
+        [TestCase]
+        public void IsmetloBeszuras()
+        {
+            lista.Beszuras(new Pokemon("Pikachu", Pokemontype.Electric, 20, false, 1, 40));
+            Assert.Throws(typeof(IlyenMarLetezikException), () => lista.Beszuras(new Pokemon("Pikachu", Pokemontype.Electric, 20, false, 1, 40)));
+        }
+        [TestCase]
+        public void BeszurasSorrend()
+        {
+            lista.Beszuras(new Pokemon("Pikachu", Pokemontype.Electric, 20, false, 1, 40));
+            lista.Beszuras(new Pokemon("Pikachu", Pokemontype.Electric, 20, false, 1, 30));
+            lista.Beszuras(new Pokemon("Pikachu", Pokemontype.Electric, 20, false, 1, 60));
+            lista.Beszuras(new Pokemon("Pikachu", Pokemontype.Electric, 20, false, 1, 10));
+            bool sorrend = true;
+            Pokemon p = lista.head;
+            while (p.Stronger!=null)
+            {
+                if (p.StrengthPoint > p.Stronger.StrengthPoint)
+                {
+                    sorrend = false;
+                }
+                p = p.Stronger;
+            }
+            Assert.That(sorrend);
+        }
+
+
+        // Kereses tesztek
+        [TestCase]
+        public void UresTorzsKereses()
+        {
+            Assert.Throws(typeof(NincsIlyenPokemonException), () => lista.Kereses("Pikachu"));
+        }
+        [TestCase]
+        public void VanolyanKereses()
+        {
+            lista.Beszuras(new Pokemon("Pikachu", Pokemontype.Electric, 20, false, 1, 40));
+            lista.Beszuras(new Pokemon("Diglet", Pokemontype.Ground, 10, false, 1, 30));
+            lista.Beszuras(new Pokemon("Balbasaur", Pokemontype.Grass, 30, false, 1, 60));
+            Assert.That(lista.Kereses("Diglet").Equals(new Pokemon("Diglet", Pokemontype.Ground, 10, false, 1, 30)));
+        }
+
+
+        // Torles tesztek
+        [TestCase]
+        public void NincsOlyanTorzsTorles()
+        {
+            Assert.Throws(typeof(NincsIlyenPokemonException), () => lista.Torles("Pikachu"));
+            
+        }
+        
+        [TestCase]
+        public void TorlesNevAlapjan()
+        {
+            lista.Beszuras(new Pokemon("Pikachu", Pokemontype.Electric, 20, false, 1, 40));
+            lista.Beszuras(new Pokemon("Diglet", Pokemontype.Ground, 10, false, 1, 30));
+            lista.Beszuras(new Pokemon("Balbasaur", Pokemontype.Grass, 30, false, 1, 60));
+            lista.Torles("Pikachu");
+            Assert.That(lista.head.Equals(new Pokemon("Diglet", Pokemontype.Ground, 10, false, 1, 30)));
+
+        }
+        [TestCase]
+        public void TorlesReferenciaAlapjan()
+        {
+            Pokemon pokemon1 = new Pokemon("Pikachu", Pokemontype.Electric, 20, false, 1, 40);
+            lista.Beszuras(pokemon1);
+            lista.Beszuras(new Pokemon("Diglet", Pokemontype.Ground, 10, false, 1, 30));
+            lista.Beszuras(new Pokemon("Balbasaur", Pokemontype.Grass, 30, false, 1, 60));
+            lista.Torles(pokemon1, false);
+            Assert.That(lista.head.Equals(new Pokemon("Diglet", Pokemontype.Ground, 10, false, 1, 30)));
+        }
+        [TestCase]
+        public void TorlesTartalomAlapjan()
+        {
+            lista.Beszuras(new Pokemon("Pikachu", Pokemontype.Electric, 20, false, 1, 40));
+            lista.Beszuras(new Pokemon("Diglet", Pokemontype.Ground, 10, false, 1, 30));
+            lista.Beszuras(new Pokemon("Balbasaur", Pokemontype.Grass, 30, false, 1, 60));
+            lista.Torles(new Pokemon("Pikachu", Pokemontype.Electric, 20, false, 1, 40), true);
+            Assert.That(lista.head.Equals(new Pokemon("Diglet", Pokemontype.Ground, 10, false, 1, 30)));
+        }
+
+
+
+        // Szures tesztek
+
+        [TestCase]
+        public void nullSzures()
+        {
+            lista.Beszuras(new Pokemon("Pikachu", Pokemontype.Electric, 20, false, 1, 40));
+            lista.Beszuras(new Pokemon("Diglet", Pokemontype.Ground, 10, false, 1, 30));
+            lista.Beszuras(new Pokemon("Balbasaur", Pokemontype.Grass, 30, false, 1, 60));
+            Assert.That(lista.Szures(Pokemontype.Bug).head == null);
+        }
+
+        [TestCase]
+        public void typeSzures()
+        {
+            lista.Beszuras(new Pokemon("Pikachu", Pokemontype.Electric, 20, false, 1, 40));
+            lista.Beszuras(new Pokemon("Balbasaur1", Pokemontype.Grass, 30, false, 1, 60));
+            lista.Beszuras(new Pokemon("Diglet", Pokemontype.Ground, 10, false, 1, 30));
+            lista.Beszuras(new Pokemon("Balbasaur2", Pokemontype.Grass, 30, false, 1, 60));
+            lista.Beszuras(new Pokemon("Balbasaur3", Pokemontype.Grass, 30, false, 1, 60));
+            PokemonLinkedList lista2 = lista.Szures(Pokemontype.Grass);
+            int i = 0;
+            Pokemon p = lista2.head;
+            while (p!=null)
+            {
+                i++;
+                p = p.Stronger;
+            }
+            Assert.That(i == 3);
+        }
+
+        [TestCase]
+        public void nevSzures()
+        {
+            lista.Beszuras(new Pokemon("Pikachu", Pokemontype.Electric, 20, false, 1, 40));
+            lista.Beszuras(new Pokemon("Balbasaur1", Pokemontype.Grass, 30, false, 1, 60));
+            lista.Beszuras(new Pokemon("Diglet", Pokemontype.Ground, 10, false, 1, 30));
+            lista.Beszuras(new Pokemon("Balbasaur2", Pokemontype.Grass, 30, false, 1, 60));
+            lista.Beszuras(new Pokemon("Balbasaur3", Pokemontype.Grass, 30, false, 1, 60));
+            PokemonLinkedList lista2 = lista.Szures("i");
+            int i = 0;
+            Pokemon p = lista2.head;
+            while (p != null)
+            {
+                i++;
+                p = p.Stronger;
+            }
+            Assert.That(i == 2);
+        }
+
+        [TestCase]
+        public void StrengthSzures()
+        {
+            lista.Beszuras(new Pokemon("Pikachu", Pokemontype.Electric, 20, false, 1, 40));
+            lista.Beszuras(new Pokemon("Balbasaur1", Pokemontype.Grass, 30, false, 1, 60));
+            lista.Beszuras(new Pokemon("Diglet", Pokemontype.Ground, 10, false, 1, 30));
+            lista.Beszuras(new Pokemon("Balbasaur2", Pokemontype.Grass, 30, false, 1, 60));
+            lista.Beszuras(new Pokemon("Balbasaur3", Pokemontype.Grass, 30, false, 1, 60));
+            PokemonLinkedList lista2 = lista.Szures(35,true);
+            int i = 0;
+            Pokemon p = lista2.head;
+            while (p != null)
+            {
+                i++;
+                p = p.Stronger;
+            }
+            Assert.That(i == 4);
+        }
+
+
+
+        //Bejaras tesztek
+        [TestCase]
+        public void Bejarasteszt()
+        {
+            lista.Beszuras(new Pokemon("Pikachu", Pokemontype.Electric, 20, false, 1, 40));
+            lista.Beszuras(new Pokemon("Balbasaur1", Pokemontype.Grass, 30, false, 1, 60));
+            lista.Beszuras(new Pokemon("Diglet", Pokemontype.Ground, 10, false, 1, 30));
+            lista.Beszuras(new Pokemon("Balbasaur2", Pokemontype.Grass, 30, false, 1, 60));
+            lista.Beszuras(new Pokemon("Balbasaur3", Pokemontype.Grass, 30, false, 1, 60));
+            lista.bejarasEvent += (Pokemon p) => p.Name = p.Name + "methodUsed";
+            lista.Bejaras();
+            bool sikerult = true;
+            Pokemon n = lista.head;
+            while (n!=null && sikerult)
+            {
+                if (!n.Name.Contains("methodUsed"))
+                {
+                    sikerult = false;
+                }
+                n = n.Stronger;
+            }
+            Assert.That(sikerult);
+            
+            
+        }
+
+        //Osszetett prog tetelek
+        [TestCase]
+        public void MetszetTeszt()
+        {
+
+            lista.Beszuras(new Pokemon("Pikachu", Pokemontype.Electric, 20, false, 1, 40));
+            lista.Beszuras(new Pokemon("Balbasaur1", Pokemontype.Grass, 30, false, 1, 60));
+            lista.Beszuras(new Pokemon("Diglet", Pokemontype.Ground, 10, false, 1, 30));
+            lista.Beszuras(new Pokemon("Balbasaur2", Pokemontype.Grass, 30, false, 1, 60));
+            lista.Beszuras(new Pokemon("Balbasaur3", Pokemontype.Grass, 30, false, 1, 60));
+
+            PokemonLinkedList lista2 = new PokemonLinkedList();
+            lista2.Beszuras(new Pokemon("Diglet", Pokemontype.Ground, 10, false, 1, 30));
+            lista2.Beszuras(new Pokemon("Eevee", Pokemontype.Normal, 30, true, 1, 40));
+            lista2.Beszuras(new Pokemon("Charmander", Pokemontype.Fire, 50, true, 1, 70));
+            lista2.Beszuras(new Pokemon("Pikachu", Pokemontype.Electric, 20, false, 1, 40));
+            lista2.Beszuras(new Pokemon("Diglet2", Pokemontype.Ground, 10, false, 1, 30));
+
+            PokemonLinkedList lista3 = lista.Metszet(lista2);
+
+            PokemonLinkedList lista4 = new PokemonLinkedList();
+            lista4.Beszuras(new Pokemon("Diglet", Pokemontype.Ground, 10, false, 1, 30));
+            lista4.Beszuras(new Pokemon("Pikachu", Pokemontype.Electric, 20, false, 1, 40));
+
+            Assert.That(lista4.head.Equals(lista3.head) && lista3.Kereses("Diglet").Equals(new Pokemon("Diglet", Pokemontype.Ground, 10, false, 1, 30)));
+            
+        }
+        [TestCase]
+        public void UnioTeszt()
+        {
+
+            lista.Beszuras(new Pokemon("Pikachu", Pokemontype.Electric, 20, false, 1, 40));
+            lista.Beszuras(new Pokemon("Balbasaur1", Pokemontype.Grass, 30, false, 1, 60));
+            lista.Beszuras(new Pokemon("Diglet", Pokemontype.Ground, 10, false, 1, 30));
+            lista.Beszuras(new Pokemon("Balbasaur2", Pokemontype.Grass, 30, false, 1, 60));
+            lista.Beszuras(new Pokemon("Balbasaur3", Pokemontype.Grass, 30, false, 1, 60));
+
+            PokemonLinkedList lista2 = new PokemonLinkedList();
+            lista2.Beszuras(new Pokemon("Diglet", Pokemontype.Ground, 10, false, 1, 30));
+            lista2.Beszuras(new Pokemon("Eevee", Pokemontype.Normal, 30, true, 1, 40));
+            lista2.Beszuras(new Pokemon("Charmander", Pokemontype.Fire, 50, true, 1, 70));
+            lista2.Beszuras(new Pokemon("Pikachu", Pokemontype.Electric, 20, false, 1, 40));
+            lista2.Beszuras(new Pokemon("Diglet2", Pokemontype.Ground, 10, false, 1, 30));
+
+            PokemonLinkedList lista3 = lista.Unio(lista2);
+
+            int i = 0;
+            Pokemon p = lista3.head;
+            while (p!=null)
+            {
+                i++;
+                p = p.Stronger;
+            }
+            Assert.That(i == 8);
+
+        }
+        [TestCase]
+        public void KulonbsegTeszt()
+        {
+
+            lista.Beszuras(new Pokemon("Pikachu", Pokemontype.Electric, 20, false, 1, 40));
+            lista.Beszuras(new Pokemon("Balbasaur1", Pokemontype.Grass, 30, false, 1, 60));
+            lista.Beszuras(new Pokemon("Diglet", Pokemontype.Ground, 10, false, 1, 30));
+            lista.Beszuras(new Pokemon("Balbasaur2", Pokemontype.Grass, 30, false, 1, 60));
+            lista.Beszuras(new Pokemon("Balbasaur3", Pokemontype.Grass, 30, false, 1, 60));
+
+            PokemonLinkedList lista2 = new PokemonLinkedList();
+            lista2.Beszuras(new Pokemon("Diglet", Pokemontype.Ground, 10, false, 1, 30));
+            lista2.Beszuras(new Pokemon("Eevee", Pokemontype.Normal, 30, true, 1, 40));
+            lista2.Beszuras(new Pokemon("Charmander", Pokemontype.Fire, 50, true, 1, 70));
+            lista2.Beszuras(new Pokemon("Pikachu", Pokemontype.Electric, 20, false, 1, 40));
+            lista2.Beszuras(new Pokemon("Diglet2", Pokemontype.Ground, 10, false, 1, 30));
+
+            PokemonLinkedList lista3 = lista.Kulonbseg(lista2);
+
+            int i = 0;
+            Pokemon p = lista3.head;
+            while (p != null)
+            {
+                i++;
+                p = p.Stronger;
+            }
+            Assert.That(i == 3);
+
+
+        }
+
+
+
+
+
+
+    }
+}
